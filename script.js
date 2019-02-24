1cd854e4c403f3f785b4edb0b81763a94e24a093
Benji Kay <okaybenji@gmail.com>
2019-02-23 18:57:22 -0600

    primary color experiment

--- script.js
+++ script.js
@@ -20,19 +20,6 @@ const dist = (x1, y1, x2, y2) => Math.sqrt( Math.pow((x1-x2), 2) + Math.pow((y1-
 
 const distFromElement = (element, x, y) => dist(x, y, element.offsetLeft + element.offsetWidth / 2, element.offsetTop + element.offsetHeight / 2);
 
-const randomIntBetween = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-const getColor = (i) => {
-  const minBrightness = 25;
-  const minSaturation = 50;
-
-  let color = 'hsl(';
-  color += (i % 180) + 120 + ',';
-  color += '100%,';
-  color += '50%)';
-  return color;
-};
-
 document.onmousemove = event => {
   const {pageX, pageY} = event;
 
@@ -40,6 +27,6 @@ document.onmousemove = event => {
     const distance = distFromElement(ripple, pageX, pageY);
     const areaOfEffect = 250;
     const rotation = scale(distance, 0, areaOfEffect, 180, 0, easeOutQuad);
-    ripple.style = `transform: rotate(${rotation}deg); color: ${getColor(i)}`;
+    ripple.style = `transform: rotate(${rotation}deg);`;
   });
 };
